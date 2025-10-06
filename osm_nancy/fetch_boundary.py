a7b7232dce7fdf033384a947eea58419fdcfb6e3
Imane Ouada <86524680+imane0x@users.noreply.github.com>
2025-10-06 14:08:05 +0200

    Delete osm_nancy/fetch_boundary.py

--- osm_nancy/fetch_boundary.py
+++
@@ -1,8 +0,0 @@
-import osmnx as ox
-
-def get_nancy_boundary():
-    """Fetch the metropolitan boundary of Nancy."""
-    gdf = ox.geocode_to_gdf("Métropole du Grand Nancy, France")
-    polygon = gdf.iloc[0].geometry
-    print("✅ Nancy metropolitan boundary loaded.")
-    return polygon
