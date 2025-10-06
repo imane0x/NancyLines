52e6e23321ab43fef0b198e8038f5958588da341
Imane Ouada <86524680+imane0x@users.noreply.github.com>
2025-10-06 14:11:46 +0200

    Update build_graph.py

--- osm_nancy/build_graph.py
+++ osm_nancy/build_graph.py
@@ -4,5 +4,5 @@ def load_graph():
     """Load the driving network graph for Nancy."""
     place = "Métropole du Grand Nancy, France"
     G = ox.graph_from_place(place, network_type="drive")
-    print("✅ OSM road network loaded.")
+    print("OSM road network loaded.")
     return G
